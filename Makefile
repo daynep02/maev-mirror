275257095eb02386c97ea6ecc002fe1b1f2fe46b
ehoyt <ethan_hoyt@taylor.edu>
2025-04-09 08:32:26 -0400

    Rebased & Updated
    Changed Python integration to use the SFML library main has switched to. Some cleanup and improvements.

--- Makefile
+++ Makefile
@@ -1,16 +1,12 @@
 INCLUDE_PYTHON = -lpython3.10 -I/usr/include/python3.10
-INCLUDE_OPENGL = -lGL -lglfw
-WIN_INCLUDE_OPENGL = -I/mingw64/include -L/mingw64/lib -lglfw3 -lopengl32 -lgdi32
 
 INCLUDE_SFML = -lsfml-graphics -lsfml-window -lsfml-system -I/usr/include/SFML/
 INCLUDE_SFML_EXTRAS = -lsfml-audio -lsfml-network
-INCLUDE_GL = -lGL -lGLU -lglut -I/usr/include/GL/
 
 .PHONY: all clean
 
-<<<<<<< HEAD
-helloworld: helloworld.cpp
-	g++ -o helloworld helloworld.cpp $(INCLUDE_SFML) $(INCLUDE_SFML_EXTRAS)
+all: helloworld.cpp
+	g++ -o engine *.cpp $(INCLUDE_PYTHON) $(INCLUDE_SFML) $(INCLUDE_SFML_EXTRAS)
 
 # win_helloworld:
 # 	g++ -o helloworld helloworld.cpp $(INCLUDE_SFML) $(INCLUDE_SFML_EXTRAS)
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,27 +1,47 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+#include <Python.h>
 
 // Example code from https://www.glfw.org/documentation.html
 
-int main(void)
-{
+int main(int argc, char* argv[]) {
+	// Python setup
+	wchar_t* program = Py_DecodeLocale(argv[0], NULL);
+	if (program == NULL) {
+		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+		exit(1);
+	}
+
+	// SFML setup
     sf::RenderWindow window(sf::VideoMode(400, 400), "Hello World!");
     sf::CircleShape shape(200.f);
     shape.setFillColor(sf::Color::Green);
 
-    while (window.isOpen())
-    {
+	// Python usage
+	Py_Initialize();
+
+	#if 1
+	PyRun_SimpleString("print('Python initialized!')");
+	#endif
+
+	// SFML usage
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
+			}
         }
 
         window.clear();
         window.draw(shape);
         window.display();
     }
-    return 0;
+	
+	#if 1
+	PyRun_SimpleString("print('Program finished!')");
+	#endif
+
+	return 0;
 }
 
