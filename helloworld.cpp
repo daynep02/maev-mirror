fa94f591b2ad9687b2dbb0c11ad7d3a608564d9a
dpeff <dayne_pefley@taylor.edu>
2025-04-09 13:00:57 -0400

    fixed sfml to 3.0.0

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,47 +1,46 @@
+#include <Python.h>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-#include <Python.h>
+#include <optional>
 
 // Example code from https://www.glfw.org/documentation.html
 
-int main(int argc, char* argv[]) {
-	// Python setup
-	wchar_t* program = Py_DecodeLocale(argv[0], NULL);
-	if (program == NULL) {
-		fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
-		exit(1);
-	}
+int main(int argc, char *argv[]) {
+  // Python setup
+  wchar_t *program = Py_DecodeLocale(argv[0], NULL);
+  if (program == NULL) {
+    fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
+    exit(1);
+  }
 
-	// SFML setup
-    sf::RenderWindow window(sf::VideoMode(400, 400), "Hello World!");
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Green);
+  // SFML setup
+  sf::RenderWindow window(sf::VideoMode({400, 400}), "Hello World!");
+  sf::CircleShape shape(200.f);
+  shape.setFillColor(sf::Color::Green);
 
-	// Python usage
-	Py_Initialize();
+  // Python usage
+  Py_Initialize();
 
-	#if 1
-	PyRun_SimpleString("print('Python initialized!')");
-	#endif
+#if 1
+  PyRun_SimpleString("print('Python initialized!')");
+#endif
 
-	// SFML usage
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-			}
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
+  // SFML usage
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
-	
-	#if 1
-	PyRun_SimpleString("print('Program finished!')");
-	#endif
 
-	return 0;
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
+#if 1
+  PyRun_SimpleString("print('Program finished!')");
+#endif
+
+  return 0;
 }
-
