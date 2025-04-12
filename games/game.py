6c2683eed27cd19a0fd84ea7d33023becd126b9a
Sterling Davis <sterling_davis1@taylor.edu>
2025-04-12 17:06:28 -0400

    Added two balls and collisions to game.py

--- games/game.py
+++ games/game.py
@@ -12,6 +12,7 @@ class Ball:
 class Game:
     def __init__(self):
         self.ball = Ball()
+        self.ball2 = Ball()
         self.background = engine.create_sprite("inspiration.jpg")
         engine.set_sprite_position(self.background,(0,0))
 
@@ -32,6 +33,15 @@ def update():
     game.ball.y = math.sin(t) * 59 + 200
     engine.set_circle_position(game.ball.circle,(game.ball.x,game.ball.y))
 
+    game.ball2.x = math.cos(0) * 50 + 200
+    game.ball2.y = math.sin(0) * 59 + 200
+    engine.set_circle_position(game.ball2.circle,(game.ball2.x,game.ball2.y))
+
+    # engine.collides_with(game.ball.circle,game.ball2.circle)
+    if engine.collides_with(game.ball.circle,game.ball2.circle):
+        print("Balls are colliding")
+    # print(engine.collides_with(game.ball.circle,game.ball2.circle))
+
 def draw():
     global game
 
@@ -39,3 +49,4 @@ def draw():
 
     engine.draw_sprite(game.background)
     engine.draw_circle(ball.circle)
+    engine.draw_circle(game.ball2.circle)
