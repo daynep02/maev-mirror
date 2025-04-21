9e7ea09f88732cc82c84b69db83035c12140b62a
Sterling Davis <sterlingdavis@lovethebrotherhood.org>
2025-04-20 22:17:46 -0400

    BoxColliders aren't a few pixels off with collisions

--- src/box_collider.cpp
+++ src/box_collider.cpp
@@ -39,10 +39,10 @@ bool BoxCollider::CollidesWith(BoxCollider* other) {
     sf::Vector2f size2 = other_box->getSize();
 
     return (
-        pos1.x < pos2.x + size2.x && 
-        pos1.x + size2.x > pos2.x &&
-        pos1.y < pos2.y + size2.y &&
-        pos1.y + size2.y > pos2.y
+        pos1.x <= pos2.x + size2.x && 
+        pos1.x + size1.x >= pos2.x &&
+        pos1.y <= pos2.y + size2.y &&
+        pos1.y + size1.y >= pos2.y
     );
 }
 
