ef55d7454a7f74848c819d404647134df959c7c8
ehoyt <ethan_hoyt@taylor.edu>
2025-04-30 08:56:30 -0400

    Added Shapes
    Created and placed basic bodies so that game logic and updating can be added later

--- games/shooter/game.py
+++ games/shooter/game.py
@@ -1,9 +1,39 @@
+import engine
+import math
+import random
+
+class Ball:
+    def __init__(self):
+        self.body = engine.create_circle(15.0)
+        self.x = 200	# should be center of screen
+        self.y = 200	# should be center of screen
+
+# game handler object
+class Game:
+	def __init__(self):
+		# TODO: no global sizes (screen height, width, etc.)
+		self.player = Ball()
+
+		# creates n enemies
+		n = 5
+		self.enemies = []
+		for i in range(n):
+			a, b = random.randint(0, 600), random.randint(0, 600)
+			self.enemies.append(engine.create_rigid_body((a, b), (20,20)))
+
 
 def init():
-	...
+	global game
+	game = Game()
 
 def update():
+	# TODO: how to move everything given camera?
+	# how to keep player centered if player is thing moving?
+	global game
 	...
 
 def draw():
-	...
+	global game
+	engine.draw_circle(game.player.body)
+	for i in game.enemies:
+		engine.draw_rigid_body_collider(i)
