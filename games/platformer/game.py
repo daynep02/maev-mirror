e02b20b93ad11e3e7d4350521d3cee47f65d3760
Sterling Davis <sterlingdavis@lovethebrotherhood.org>
2025-05-02 08:06:42 -0400

    Added zone for collision (wip)

--- games/platformer/game.py
+++ games/platformer/game.py
@@ -11,9 +11,13 @@ class Game:
         self.platforms = [engine.create_rigid_body((0,300), (100,20))]
         self.platforms.append(engine.create_rigid_body((150, 350), (500,10)))
         self.platforms.append(engine.create_rigid_body((660, 250), (200,10)))
+        self.zone1 = engine.create_box_collider((660, 250), (200,10))
 
-        engine.set_gravity(0.0, 50.0)
-        engine.set_terminal_velo(0.0, 1000.0)
+        engine.set_rigid_body_static(self.player, False)
+        engine.set_rigid_body_gravity(self.player, True)
+
+        engine.set_gravity(0.0, 1.0)
+        engine.set_terminal_velo(self.player, 200.0, 200.0)
 
         for platform in self.platforms:
             engine.set_rigid_body_static(platform,True)
@@ -21,19 +25,26 @@ class Game:
 def init():
     global game
     game = Game()
-    print(game.platforms)
 
 def update():
     global game
     delta = engine.delta_time()
 
-    engine.set_rigid_body_velocity(game.player, (0.0, 0.0))
-    if engine.key_is_pressed(AKEY):
-        engine.set_rigid_body_velocity(game.player, (-50.0, 0.0))
-        # print("A pressed")
-    if engine.key_is_pressed(DKEY):
-        engine.set_rigid_body_velocity(game.player, (50.0, 0.0))
-        # print("D pressed")
+    # engine.set_rigid_body_velocity(game.player, (0.0, 0.0))
+    # if engine.key_is_pressed(AKEY):
+    #     engine.set_rigid_body_velocity(game.player, (-50.0, 0.0))
+    #     # print("A pressed")
+    # if engine.key_is_pressed(DKEY):
+    #     engine.set_rigid_body_velocity(game.player, (50.0, 0.0))
+    #     # print("D pressed")
+    if engine.key_is_pressed("Up"):
+        engine.apply_force(game.player, 0.0, -2.0)
+    if engine.key_is_pressed( "Down"):
+        engine.apply_force(game.player, 0.0, 2.0)
+    if engine.key_is_pressed("Left"):
+        engine.apply_force(game.player,-2.0, 0.0)
+    if engine.key_is_pressed("Right"):
+        engine.apply_force(game.player, 1.0, 0.0)
 
     tup = engine.get_rigid_body_position(game.player)
     engine.set_camera_position(tup)
@@ -42,6 +53,7 @@ def draw():
     global game
 
     engine.draw_rigid_body_collider(game.player)
+    # engine.draw
 
     for platform in game.platforms:
         engine.draw_rigid_body_collider(platform)
