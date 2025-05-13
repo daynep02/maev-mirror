3783b3a6c5db4eb0521bbdfed6f4256a6b3026a5
ehoyt <ethan_hoyt@taylor.edu>
2025-05-12 22:48:35 -0400

    Improved Enemies
    Enemies now have significantly more interesting, involved, and random AI. Also added walls and drafted a simple level design.

--- games/shooter/game.py
+++ games/shooter/game.py
@@ -42,25 +42,43 @@ class Player:
 
 class Enemy:
     def __init__(self):
-        self.x, self.y = random.randint(0, 2000), random.randint(0, 2000)
+        # actual physical positioning
+        self.x, self.y = random.randint(-295, 1280), random.randint(-595, -20)
         self.body = engine.create_rigid_body((self.x, self.y), (15,15))
+
+        # friction and velocity
+        self.dx = 0
+        self.dy = 0
+        #engine.set_terminal_velo(self.body, 1.0, 1.0)
+        engine.set_rigid_body_static(self.body, False)
+        engine.set_rigid_body_gravity(self.body, False)
+
+        # other traits
         self.health = 1000
         self.seek_player = False
 
     def move(self):
-        # pursue player directly
+        # between player and enemy
+        dist_from_player = engine.create_vector(self.x - game.player.x, self.y - game.player.y)
+
+        # pursue player directly: change velocity based on dist from player
+        # all numbers fairly arbitrary: base attraction speed * random aggression * magnet to player
         if self.seek_player:
+            aggro = random.randint(1, 100)
             if self.x > game.player.x:
-                self.x -= 0.05
+                self.dx -= 0.001 * (5 / aggro) * (20 / engine.length(dist_from_player))
             elif self.x < game.player.x:
-                self.x += 0.05
+                self.dx += 0.001 * (5 / aggro) * (20 / engine.length(dist_from_player))
             if self.y > game.player.y:
-                self.y -= 0.05
+                self.dy -= 0.001 * (5 / aggro) * (20 / engine.length(dist_from_player))
             elif self.y < game.player.y:
-                self.y += 0.05
+                self.dy += 0.001 * (5 / aggro) * (20 / engine.length(dist_from_player))
+            engine.set_rigid_body_velocity(self.body, (self.dx, self.dy))
         
-        # like an idle animation
+        # like an idle animation: slightly tweak absolute position
         else:
+            self.dx = 0
+            self.dy = 0
             i = random.randint(0, 100)
             if i == 1:
                 self.x -= 0.05
@@ -70,6 +88,10 @@ class Enemy:
                 self.y -= 0.05
             elif i == 4:
                 self.y += 0.05
+
+        # change enemy's absolute position considering velocity (so works for either mode)
+        self.x += self.dx
+        self.y += self.dy
         engine.set_rigid_body_position(self.body, (self.x, self.y))
 
 
@@ -82,6 +104,18 @@ class Game:
         self.enemies = []
         for i in range(n):
             self.enemies.append(Enemy())
+
+        self.walls = []
+        self.walls.append(engine.create_rigid_body((300, 0), (2, 500)))
+        self.walls.append(engine.create_rigid_body((300, 500), (400, 2)))
+        self.walls.append(engine.create_rigid_body((700, 0), (2, 500)))
+
+        self.walls.append(engine.create_rigid_body((-300, 0), (600,2)))
+        self.walls.append(engine.create_rigid_body((700, 0), (600,2)))
+        self.walls.append(engine.create_rigid_body((-300, -600), (2, 600)))
+        self.walls.append(engine.create_rigid_body((1300, -600), (2, 600)))
+        self.walls.append(engine.create_rigid_body((-300, -600), (1600, 2)))
+
 
 
 def init():
@@ -106,12 +140,14 @@ def update():
 
     for i in game.enemies:
         # how close enemies need to be to pursue
-        crit_dist_x = (NEW_SCREEN_SIZE[0] / 10)
-        crit_dist_y = (NEW_SCREEN_SIZE[1] / 10)
+        crit_dist_x = (NEW_SCREEN_SIZE[0] / 5)
+        crit_dist_y = (NEW_SCREEN_SIZE[1] / 5)
 
         if ((game.player.x - crit_dist_x) <= i.x <= (game.player.x + crit_dist_x)) and \
         ((game.player.y - crit_dist_y) <= i.y <= (game.player.y + crit_dist_y)):
             i.seek_player = True
+        else:
+            i.seek_player = False
         i.move()
 
     # keep camera centered on player
@@ -131,8 +167,11 @@ def update():
 
 
 def draw():
-	global game
-	engine.draw_circle(game.player.body)
-     
-	for i in game.enemies:
-		engine.draw_rigid_body_collider(i.body)
+    global game
+    engine.draw_circle(game.player.body)
+
+    for i in game.enemies:
+        engine.draw_rigid_body_collider(i.body)
+
+    for i in game.walls:
+        engine.draw_rigid_body_collider(i)
