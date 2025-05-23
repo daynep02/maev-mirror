ff1f89505258b5e86c501e3bc60cdf8fa028c188
ehoyt <ethan_hoyt@taylor.edu>
2025-05-22 23:11:17 -0400

    Added Menu and States
    Game now contains main menu and game over screens.

--- games/shooter/game.py
+++ games/shooter/game.py
@@ -7,8 +7,6 @@ NEW_SCREEN_SIZE = (1920, 1080)
 
 # TODO: enemies
 # TODO: levels
-# TODO: title screen
-# TODO: player health, weapon types, etc.
 
 class Player:
     def __init__(self):
@@ -54,7 +52,7 @@ class Enemy:
         engine.set_rigid_body_gravity(self.body, False)
 
         # other traits
-        self.health = 1000
+        self.health = 10000
         self.seek_player = False
 
     def move(self):
@@ -100,11 +98,13 @@ class Game:
     def __init__(self):
         self.player = Player()
 
+        # enemy generation
         n = 15
         self.enemies = []
         for i in range(n):
             self.enemies.append(Enemy())
 
+        # wall generation
         self.walls = []
         self.walls.append(engine.create_rigid_body((300, 0), (2, 500)))
         self.walls.append(engine.create_rigid_body((300, 500), (400, 2)))
@@ -121,78 +121,134 @@ class Game:
 def init():
     engine.set_screen_size(NEW_SCREEN_SIZE[0], NEW_SCREEN_SIZE[1])
     global game
+    global state
+    global ratio_w
+    global ratio_h
     global l
     global bullet
     
     game = Game()
+    state = 0
+    ratio_w = NEW_SCREEN_SIZE[0] / ORIG_SCREEN_SIZE[0]
+    ratio_h = NEW_SCREEN_SIZE[1] / ORIG_SCREEN_SIZE[1]
+    bullet = engine.create_vector()
 
 
 def update():
-    # TODO: fire vectors
     global game
+    global state
+    global ratio_w
+    global ratio_h
     global l
     global bullet
-     
-    # player movement
-    if engine.key_is_pressed("W"):
-        game.player.move("up")
-    elif engine.key_is_pressed("S"):
-        game.player.move("down")
-    if engine.key_is_pressed("A"):
-        game.player.move("left")
-    elif engine.key_is_pressed("D"):
-        game.player.move("right")
 
-    for i in game.enemies:
-        # how close enemies need to be to pursue
-        crit_dist_x = (NEW_SCREEN_SIZE[0] / 5)
-        crit_dist_y = (NEW_SCREEN_SIZE[1] / 5)
-
-        if ((game.player.x - crit_dist_x) <= i.x <= (game.player.x + crit_dist_x)) and \
-        ((game.player.y - crit_dist_y) <= i.y <= (game.player.y + crit_dist_y)):
-            i.seek_player = True
-        else:
-            i.seek_player = False
-        i.move()
-
-    # keep camera centered on player
-    engine.set_camera_position((game.player.x, game.player.y))
-
-    # player firing vectors
-    l = engine.mouse_button_is_pressed("Left")
-    if l > 0:
-        # ensure mouse click is within screen bounds
-        if 0 <= engine.x(l) <= NEW_SCREEN_SIZE[0] and 0 <= engine.y(l) <= NEW_SCREEN_SIZE[1]:
-            bullet = engine.create_vector(engine.x(l), engine.y(l))
-            game.player.fire(l)
+    # main menu
+    if state == 0:
     
-    else:
-        engine.cleanse_vectors()
+        l = engine.mouse_button_is_pressed("Left")
+        if l > 0:
+            if (NEW_SCREEN_SIZE[0] * 0.2 <= engine.x(l) <= NEW_SCREEN_SIZE[0] * 0.8) and \
+            (NEW_SCREEN_SIZE[1] * 0.2 <= engine.y(l) <= NEW_SCREEN_SIZE[1] * 0.4):
+                state = 1
+
+            elif (NEW_SCREEN_SIZE[0] * 0.2 <= engine.x(l) <= NEW_SCREEN_SIZE[0] * 0.8) and \
+            (NEW_SCREEN_SIZE[1] * 0.6 <= engine.y(l) <= NEW_SCREEN_SIZE[1] * 0.8):
+                exit(0)
+
+    # game
+    elif state == 1:
+        # player movement
+        if engine.key_is_pressed("W"):
+            game.player.move("up")
+        elif engine.key_is_pressed("S"):
+            game.player.move("down")
+        if engine.key_is_pressed("A"):
+            game.player.move("left")
+        elif engine.key_is_pressed("D"):
+            game.player.move("right")
+
+        for i in game.enemies:
+            # how close enemies need to be to pursue
+            crit_dist_x = (NEW_SCREEN_SIZE[0] / 5)
+            crit_dist_y = (NEW_SCREEN_SIZE[1] / 5)
+
+            # switch to pursuing
+            if ((game.player.x - crit_dist_x) <= i.x <= (game.player.x + crit_dist_x)) and \
+            ((game.player.y - crit_dist_y) <= i.y <= (game.player.y + crit_dist_y)):
+                i.seek_player = True
+            else:
+                i.seek_player = False
+            i.move()
+
+            # damage
+            if game.player.x - 15.0 <= i.x <= game.player.x + 5.0 and game.player.y - 15.0 <= i.y <= game.player.y + 5.0:
+                game.player.health -= 1
+
+        # player death
+        if game.player.health <= 0:
+            state = 2
+
+        # keep camera centered on player
+        engine.set_camera_position((game.player.x, game.player.y))
+
+        # player firing vectors
+        l = engine.mouse_button_is_pressed("Left")
+        if l > 0:
+            # ensure mouse click is within screen bounds
+            if 0 <= engine.x(l) <= NEW_SCREEN_SIZE[0] and 0 <= engine.y(l) <= NEW_SCREEN_SIZE[1]:
+                bullet = engine.create_vector(engine.x(l), engine.y(l))
+                game.player.fire(l)
+        
+        else:
+            engine.cleanse_vectors()
+    
+    # game over screen
+    elif state == 2:
+        ...
 
 
 def draw():
     global game
+    global state
+    global ratio_w
+    global ratio_h
     global l
     global bullet
 
-    engine.draw_circle(game.player.body)
+    # main menu
+    if state == 0:
+        # play button
+        r = engine.create_rect((NEW_SCREEN_SIZE[0] * 0.6 / ratio_w, NEW_SCREEN_SIZE[1] * 0.2 / ratio_h))
+        engine.set_rect_position(r, (NEW_SCREEN_SIZE[0] * 0.2 / ratio_w, NEW_SCREEN_SIZE[1] * 0.2 / ratio_h))
+        engine.set_rect_fill_color(r, (255,255,255,255))
+        engine.draw_rect(r)
 
-    # fired bullets
-    if l > 0:
-        ratio_w = NEW_SCREEN_SIZE[0] / ORIG_SCREEN_SIZE[0]
-        ratio_h = NEW_SCREEN_SIZE[1] / ORIG_SCREEN_SIZE[1]
-    
-        
-        cx, cy = (game.player.x + 5.0 * ratio_w), (game.player.y + 5.0 * ratio_h)
-        # keeps end of Vector proportional to both mouse and player positions
-        mx, my = cx + ((engine.x(l) - NEW_SCREEN_SIZE[0] / 2) * ratio_w), cy + ((engine.y(l) - NEW_SCREEN_SIZE[1] /2) * ratio_h)
-        
-        engine.draw_vector(bullet, (cx, cy), (mx, my))
+        # exit button
+        e = engine.create_rect((NEW_SCREEN_SIZE[0] * 0.6 / ratio_w, NEW_SCREEN_SIZE[1] * 0.2 / ratio_h))
+        engine.set_rect_position(e, (NEW_SCREEN_SIZE[0] * 0.2 / ratio_w, NEW_SCREEN_SIZE[1] * 0.6 / ratio_h))
+        engine.set_rect_fill_color(e, (255,50,50,255))
+        engine.draw_rect(e)
 
-    # enemies
-    for i in game.enemies:
-        engine.draw_rigid_body_collider(i.body)
+    # game
+    elif state == 1:
+        engine.draw_circle(game.player.body)
 
-    # walls
-    for i in game.walls:
-        engine.draw_rigid_body_collider(i)
+        # fired bullets
+        if l > 0:        
+            cx, cy = (game.player.x + 5.0 * ratio_w), (game.player.y + 5.0 * ratio_h)
+            # keeps end of Vector proportional to both mouse and player positions
+            mx, my = cx + ((engine.x(l) - NEW_SCREEN_SIZE[0] / 2) * ratio_w), cy + ((engine.y(l) - NEW_SCREEN_SIZE[1] /2) * ratio_h)
+            
+            engine.draw_vector(bullet, (cx, cy), (mx, my))
+
+        # enemies
+        for i in game.enemies:
+            engine.draw_rigid_body_collider(i.body)
+
+        # walls
+        for i in game.walls:
+            engine.draw_rigid_body_collider(i)
+
+    # game over
+    elif state == 2:
+        ...
